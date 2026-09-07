5e2b490d7e8009aa0b2141371770ae0c4d075697
reaue <mathias.crama.second@gmail.com>
2026-09-07 23:39:28 +0200

    adding a Top Bar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 function updateHourDate () {
     let date = new Date();
     // const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    document.getElementById("hour-date").innerHTML = date.toLocaleString('en-GB');
+    document.getElementById("hour").innerHTML = date.toLocaleTimeString('en-GB', {hour: "2-digit", minute: "2-digit"});
+    document.getElementById("date").innerHTML = date.toLocaleDateString('en-GB');
 };
 
 updateHourDate();
